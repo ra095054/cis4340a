0cfc5e5f7ca172d3cee41862eebe1c37e4dee678
ra095054 <ra095054@ucf.edu>
2026-03-03 12:57:59 -0500

    Update Cis4340.java

--- Cis4340.java
+++ Cis4340.java
@@ -3,7 +3,7 @@
  *  Execution:    java Cis4340
  *
  *  Prints 
- *    "Hello, World - from the class of cis4340 - Spring 2024 - UCF (your name here_)".
+ *    "Hello, World - from the class of cis4340 - Spring 2024 - UCF (Rachel Hart)".
  *  By tradition, this is everyone's first program.
  *
  * Revision History:
