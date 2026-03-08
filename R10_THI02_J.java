75c5e90d644c38c6f0f12aeec98b63bfb1689ce5
ra095054 <ra095054@ucf.edu>
2026-03-08 13:52:22 -0400

    Create R10_THI02_J.java

---
+++ R10_THI02_J.java
@@ -0,0 +1,31 @@
+public final class ProcessStep implements Runnable {
+  private static final Object lock = new Object();
+  private static int time = 0;
+  private final int step; // Do perform operations when field time reaches this value
+
+public ProcessStep(int step) {
+  this.step = step;
+}
+
+@Override public void run() {
+  try {
+    synchronized (lock) {
+      while (time != step) {
+        lock.wait();
+      }
+      //Perfrom operations
+
+    time++;
+      lock.notify();
+    }
+  } catch (InterruptedException ie) {
+    Thread.currentThread().interrupt(); // Reset interrupted status
+  }
+}
+
+public static void main(String[] args) {
+  for (int i = 4; i >= 0; i--) {
+    new Thread(new ProcessStep(i)).start();
+  }
+}
+}
