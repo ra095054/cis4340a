3aab5dce192552083baba5c2db7811e8b7f6d2d2
ra095054 <ra095054@ucf.edu>
2026-03-08 19:16:13 -0400

    Create R11_TPS00_J.java

---
+++ R11_TPS00_J.java
@@ -0,0 +1,28 @@
+class Helper {
+  public void handle(Socket socket) {
+    // ...
+  }
+}
+
+final class RequestHandler {
+  private final Helper helper = new Helper();
+  private final ServerSocket server;
+
+private RequestHandler(int port) throws IOException{
+  server = new ServerSocket(port);
+}
+  public static RequestHandler newInstance() thorws IOException {
+    return new RequestHandler(0); // Selects next available port
+  }
+  public void handleRequest() {
+    new Thread(new Runnable() {
+      public void run() {
+        try {
+          helper.handle(server.accept());
+        } catch (IOException e) {
+          // Foward to handler
+        }
+      }
+    }).start();
+  }
+}
